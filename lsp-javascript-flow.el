b27281fa3a922b427563df53c87ed2911ecc0f95
Steven Rémot <steven.remot@gmail.com>
2017-12-31 14:57:22 +0100

    Write the first version

---
+++ lsp-javascript-flow.el
@@ -0,0 +1,26 @@
+;;; lsp-javascript-flow.el --- Language server protocol integration for flow
+;;; Version: 0.1.0
+;;; URL: None
+;;; Package-Requires: ((lsp-mode "3.4"))
+
+;;; Commentary:
+;; This package provides editor support for flow via lsp-mode and the
+;; flow-language-server.
+;;
+;; Be sure to install flow-langauge-server (npm i -g flow-language-server)
+;; before using it.
+;;; Code:
+(require 'lsp-mode)
+
+(defconst lsp-javascript-flow--get-root
+  (lsp-make-traverser #'(lambda (dir)
+                          (directory-files dir nil ".flowconfig"))))
+
+;;;###autoload
+(lsp-define-stdio-client lsp-javascript-flow "javascript-flow"
+                         lsp-javascript-flow--get-root
+                         '("flow-language-server" "--stdio"))
+
+(provide 'lsp-javascript-flow)
+
+;;; lsp-javascript-flow.el ends here
